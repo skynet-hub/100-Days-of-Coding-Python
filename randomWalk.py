001cfb036a1809cca3382124ef8294410f311464
Pablo <dave@wltcode.com>
2024-07-24 16:24:10 +0200

    using rgb to enhance random walk project

--- randomWalk.py
+++ randomWalk.py
@@ -2,7 +2,15 @@ import turtle
 import random
 
 timmy = turtle.Turtle()
-colors = ['red', 'blue', 'purple', 'pink', 'brown', 'yellow', 'grey', 'green']
+turtle.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+
+    random_color = (r, g, b)
+    return random_color
 
 direction = [0, 90, 180, 270]
 timmy.pensize(11)
@@ -11,8 +19,7 @@ timmy.speed('fast')
 
 for _ in range(200):
    
-    color = random.choice(colors)
-    timmy.color(color)
+    timmy.color(random_color())
     timmy.forward(30)
     timmy.setheading(random.choice(direction))
 
